cbce1c31f484468284fc3cf1f3f3f10fcdba738f
Fpsska <fpsska1337@gmail.com>
2023-05-12 14:08:29 +0300

    feat: create style, _vars, _fonts, _reset scss-files & create images directory, add styles for body HTML-el

--- src/app/components/layout/App/App.tsx
+++ src/app/components/layout/App/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
 import './App.css';
+import 'assets/styles/style.scss';
 
 // /. imports
 
 const App: React.FC = () => {
-    return <div className="App">Hello world!</div>;
+    return (
+        <div className="App">
+            Hello world!
+            <div></div>
+        </div>
+    );
 };
 
 export default App;
